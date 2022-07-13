561e7693860b7253853e010c60456b8c68816a4c
s1290106 <s1290106@u-aizu.ac.jp>
2022-07-13 23:33:51 +0900

    HoT: initial revision

---
+++ practice.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int count = 0, num;
+  printf("Tossing a coin...\n");
+
+  srand((unsigned int)time(NULL));
+  
+  for(int i = 0; i < 3; i++){
+    num = rand() % 2;
+    if(num == 0){
+      printf("Round %d: Heads\n",i+1);
+      count++;
+    }
+    else if(num == 1){
+      printf("Round %d: Tails\n",i+1);
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",count,3-count);
+}
